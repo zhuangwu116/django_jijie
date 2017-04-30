60704becdc5d18c0bbc6387c370d0abceea18814
zhuangwu <562669088@qq.com>
2017-04-30 23:44:33 +0800

    自定义tag

--- demo/templatetags/poll_extras.py
+++ demo/templatetags/poll_extras.py
@@ -3,25 +3,28 @@ from django import template
 from datetime import datetime
 register=template.Library()
 
-class AllenDateNode(template.Node):
-    def __init__(self,format_string,asvar):
-        self.format_string=format_string
-        self.asvar=asvar
-
-    def render(self, context):
-        now=datetime.now().strftime(self.format_string)
-        if self.asvar:
-            context[self.asvar]=now
-            return ""
-        else:
-            return now
-@register.tag(name='dateAllen')
-def dateAllen(parse,token):
-    args=token.split_contents()
-    asvar=None
-    if len(args)==4 and args[-2]=='as':
-        asvar=args[-1]
-    elif len(args)!=2:
-        raise template.TemplateSyntaxError('invalid args error')
-    return AllenDateNode(args[1][1:-1],asvar)
+# class AllenDateNode(template.Node):
+#     def __init__(self,format_string,asvar):
+#         self.format_string=format_string
+#         self.asvar=asvar
+#
+#     def render(self, context):
+#         now=datetime.now().strftime(self.format_string)
+#         if self.asvar:
+#             context[self.asvar]=now
+#             return ""
+#         else:
+#             return now
+# @register.tag()
+# def dateAllen(parse,token):
+#     args=token.split_contents()
+#     asvar=None
+#     if len(args)==4 and args[-2]=='as':
+#         asvar=args[-1]
+#     elif len(args)!=2:
+#         raise template.TemplateSyntaxError('invalid args error')
+#     return AllenDateNode(args[1][1:-1],asvar)
 # register.tag(name='dateAllen',compile_function=dateAllen)
+@register.assignment_tag()
+def get_current_time(format_string):
+    return datetime.now().strftime(format_string)

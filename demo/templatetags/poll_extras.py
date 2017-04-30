3664cc12fd188a542183684153c99a8eb78410bb
zhuangwu <562669088@qq.com>
2017-04-30 23:22:57 +0800

    自定义tag

---
+++ demo/templatetags/poll_extras.py
@@ -0,0 +1,18 @@
+from django import template
+
+from datetime import datetime
+register=template.Library()
+
+class AllenDateNode(template.Node):
+    def __init__(self,format_string):
+        self.format_string=format_string
+    def render(self, context):
+        return datetime.now().strftime(self.format_string)
+@register.tag(name='dateAllen')
+def dateAllen(parse,token):
+    try:
+        tag_name,format_string=token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
+    return AllenDateNode(format_string[1:-1])
+# register.tag(name='dateAllen',compile_function=dateAllen)

ef0be0bc4750d7fce09536c8a95db07f8d81bc44
zhuangwu <562669088@qq.com>
2017-05-20 19:08:18 +0800

    django 信号

--- demo/signals.py
+++ demo/signals.py
@@ -1,10 +1,10 @@
-from django.dispatch import Signal
+from django.dispatch import Signal,receiver
 signalAllen=Signal(providing_args=['allen'])
+@receiver(signalAllen)
 def signal_callback(sender,**kwargs):
     print(sender,kwargs)
     print('signal_callback called')
+@receiver(signalAllen)
 def signal_callback1(sender,**kwargs):
     print(sender,kwargs)
     print('signal_callback called1')
-signalAllen.connect(signal_callback)
-signalAllen.connect(signal_callback1)
